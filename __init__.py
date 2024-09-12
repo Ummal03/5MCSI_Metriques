295c64ff90118894793912ba3cd114033dd3e7ca
Ummal03 <uluu1@myges.fr>
2024-09-12 15:36:58 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -27,7 +27,7 @@ def meteo():
 def mongraphique():
     return render_template("graphique.html")
 
-@app.route("/histogrramme/")
+@app.route("/histogramme/")
 def monhistogramme():
     return render_template("histogramme.html")
   
