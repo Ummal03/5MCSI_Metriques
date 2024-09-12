cf3e02a7a3a32e49fdad949d1f93c4fae47a51ff
Ummal03 <uluu1@myges.fr>
2024-09-12 14:42:57 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
                                                                                                                                        
 @app.route('/')
 def hello_world():
-    return render_template('hello.html') #TESTACTION
+    return render_template('hello.html') #TEST
   
 if __name__ == "__main__":
   app.run(debug=True)
